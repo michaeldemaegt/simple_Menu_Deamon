3dda8474f2ad9d0e481a151e826ecb2b34de6a20
michael <michael@savvyzebra.com>
2024-08-17 12:54:50 +0200

    Cleaned-up

--- R/Global Variables.R
+++ R/Global Variables.R
@@ -1,3 +1,5 @@
+#' Global Variables
+
 # Raw data file of the recipes
 xlsxFile <- "Data/Source recepten.xlsx"
 
@@ -17,7 +19,7 @@ week_day_name <- tribble(
 
 #Email adress to send to
 from = "info@savvyzebra.com"
-to = "mdemaegt@yahoo.com"
+to <- c("mdemaegt@yahoo.com","mdemaegt@icloud.com")
 
 # HTML Header and Footer
 header <- '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
@@ -35,24 +37,5 @@ Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 footer <- '</body>
           </html>'
 
-
-# HTML Header and Footer
-test <- '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
-Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>HTML demo</title>
-        <style type="text/css">
-          </style>
-          </head>
-          <body>TEST</body>
-          </html>'
-
-
-mike <- paste0(header,"TEST",footer)
-
-mike==test
 
 
